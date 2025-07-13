8b404ce0f86e6fded0f69a70f2a0ffe3ec77defb
asray <kmkmgwt@yahoo.co.jp>
2025-07-13 09:10:34 +0900

    static test 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from .models import User
+import os
 
 from .models import db
 login_manager = LoginManager()
 
 def create_app():
+    print(os.getcwd())
     app = Flask(__name__, static_folder='./static/')
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLiteデータベース
