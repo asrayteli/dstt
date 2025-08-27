4164f25c9659bf9cf8c2fd844824d6f67b1c5779
asrayteli <kmkmgwt@yahoo.co.jp>
2025-08-28 07:31:32 +0900

    SS options update

--- dstt/Scripts/activate.bat
+++ dstt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=H:\DSTT\dstt\dstt
+set VIRTUAL_ENV=C:\Users\kmkm_asd\Desktop\dstt\dstt
 
 if not defined PROMPT set PROMPT=$P$G
 
